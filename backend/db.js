6772414b4920ce094342af6f99ba74422d618a77
Leighton A Pitman <leightonpitman14@gmail.com>
2020-06-28 18:21:53 -0400

    add authentication for admins

---
+++ backend/db.js
@@ -0,0 +1,95 @@
+const { Client } = require("pg");
+const bcrypt = require("bcrypt");
+const jwt = require("jwt-simple");
+
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost/blog_db",
+);
+
+client.connect();
+
+const sync = async () => {
+  const SQL = `
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  DROP TABLE IF EXISTS users CASCADE;
+  CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    CHECK (char_length(username) > 0)
+  );
+  `;
+  await client.query(SQL);
+  const [moe, lucy, curly] = await Promise.all([
+    createUser({ username: "lpitman", password: "leighton" }),
+    createUser({ username: "lucy", password: "LUCY" }),
+    // createUser({ username: "curly", password: "CURLY" }),
+  ]);
+
+  const token = await authenticate({ username: "lucy", password: "LUCY" });
+  console.log("TOKEN:", token);
+  const user = await getUserFromToken(token);
+  console.log(user);
+};
+
+const readUsers = async () => {
+  return (await client.query("SELECT * FROM users")).rows;
+};
+
+const getUserFromToken = async (token) => {
+  const id = jwt.decode(token, process.env.JWT).id;
+  const user = (await client.query("SELECT * FROM users WHERE id=$1", [id]))
+    .rows[0];
+  return user;
+};
+
+const authenticate = async ({ username, password }) => {
+  const user = (
+    await client.query("SELECT * FROM users WHERE username=$1", [username])
+  ).rows[0];
+  await compare({ plain: password, hashed: user.password });
+
+  return jwt.encode({ id: user.id }, process.env.JWT);
+};
+
+const compare = async ({ plain, hashed }) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(plain, hashed, (err, success) => {
+      if (err) {
+        return reject(err);
+      }
+      if (success) {
+        return resolve();
+      }
+      reject({ message: "incorrect username or password" });
+    });
+  });
+};
+
+const createUser = async ({ username, password }) => {
+  const hashed = await hash(password);
+  return (
+    await client.query(
+      "INSERT INTO users(username, password) values ($1, $2) returning *",
+      [username, hashed],
+    )
+  ).rows[0];
+};
+
+const hash = (plain) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(plain, 10, (err, hashed) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(hashed);
+    });
+  });
+};
+
+module.exports = {
+  sync,
+  authenticate,
+  getUserFromToken,
+  readUsers,
+};
--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,19 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function LoginPage() {
+  const [auth, setAuth] = useState({});
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    attemptLoginFromToken();
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const credentials = {
+      username,
+      password,
+    };
+
+    axios
+      .post("/api/auth", credentials)
+      .then((response) => {
+        window.localStorage.setItem("token", response.data.token);
+        attemptLoginFromToken();
+      })
+      .catch((ex) => setError(ex.response.data.message));
+  };
+
+  const attemptLoginFromToken = () => {
+    const token = window.localStorage.getItem("token");
+    if (!token) {
+      return;
+    }
+    if (!token) {
+      return;
+    }
+    axios
+      .get("/api/auth", {
+        headers: {
+          authentication: token,
+        },
+      })
+      .then((response) => setAuth(response.data));
+  };
+
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    setAuth({});
+  };
+
   return (
     <div id="login-page">
-      <h2>Administrator login</h2>
-      <br />
-      <form id="login-form">
-        <label>Username</label>
-        <input className="text-field" type="text"></input>
-        <br />
-        <label>Password</label>
-        <input className="text-field" type="password"></input>
-        <br />
-        <input id="submit-button" type="submit" value="Sign in" />
-      </form>
+      {!auth.id ? (
+        <div>
+          <h2>Administrator login</h2>
+          <br />
+          <p>{error}</p>
+          <form id="login-form" onSubmit={onSubmit}>
+            <label>Username</label>
+            <input
+              className="text-field"
+              type="text"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <br />
+            <label>Password</label>
+            <input
+              className="text-field"
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <br />
+            <input id="submit-button" type="submit" value="Sign in" />
+          </form>
+        </div>
+      ) : (
+        <div>
+          <button onClick={logout}>log out {username}</button>
+        </div>
+      )}
     </div>
   );
 }
