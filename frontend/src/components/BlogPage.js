e7e62740db42c3ece4b5a1b4882330346d875418
Leighton A Pitman <leightonpitman14@gmail.com>
2020-07-10 19:08:42 -0400

    begin building out projects page

--- frontend/src/components/BlogPage.js
+++ frontend/src/components/BlogPage.js
@@ -1,30 +1,56 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-  useRouteMatch,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Route, useRouteMatch } from "react-router-dom";
 import BlogEntry from "./BlogEntry";
 import blogs from "../blogPosts.json";
 
-export default function BlogPage() {
-  let { path, url } = useRouteMatch();
+export default function BlogPage({ selectedPost, setSelectedPost }) {
+  let { url } = useRouteMatch();
+
+  useEffect(() => {
+    if (!isNaN(url.slice(url.lastIndexOf("/") + 1))) {
+      setSelectedPost(url.slice(url.lastIndexOf("/") + 1));
+    } else {
+      setSelectedPost("");
+    }
+  }, []);
+
+  const handleClick = (blogId) => {
+    setSelectedPost(blogId);
+  };
 
   return (
     <div>
-      <ul className="blog-links-container">
-        {blogs.map((blog) => {
-          return (
-            <Link className="blog-link" to={`${url}/${blog.id}`}>
-              <li key={blog.id}>{blog.title}</li>
-            </Link>
-          );
-        })}
-      </ul>
-
-      <Route path={`${url}/:entryId`} component={BlogEntry} />
+      {!selectedPost ? (
+        <ul className="blog-links-container">
+          {blogs.map((blog) => {
+            return (
+              <li
+                style={{ listStyleType: "none", margin: "0.25rem" }}
+                key={blog.id}
+              >
+                <Link
+                  className="blog-link"
+                  onClick={() => handleClick(blog.id)}
+                  to={`${url}/${blog.id}`}
+                >
+                  {blog.date}{" "}
+                  <strong style={{ marginLeft: "2rem" }}>{blog.title}</strong>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div>
+          <Route path={`${url}/:entryId`} component={BlogEntry} />
+          <div
+            className="floating-back-button"
+            onClick={() => setSelectedPost(false)}
+          >
+            <p>{"<"}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/ProjectsPage.js
+++ frontend/src/components/ProjectsPage.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, Route, useRouteMatch } from "react-router-dom";
+import ProjectEntry from "./ProjectEntry";
+import projects from "../projects.json";
 
-export default function ProjectsPage() {
+export default function ProjectsPage({ selectedProject, setSelectedProject }) {
+  let { url } = useRouteMatch();
+
+  const handleClick = (projectId) => {
+    setSelectedProject(projectId);
+  };
+
   return (
     <div>
-      <h2>Projects</h2>
+      {!selectedProject ? (
+        <ul className="blog-links-container">
+          {projects.map((project) => {
+            return (
+              <li
+                style={{ listStyleType: "none", margin: "0.25rem" }}
+                key={project.id}
+              >
+                <Link
+                  className="blog-link"
+                  onClick={() => handleClick(project.id)}
+                  to={`${url}/${project.id}`}
+                >
+                  <strong style={{ marginLeft: "2rem" }}>
+                    {project.title}
+                  </strong>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div>
+          <Route path={`${url}/:entryId`} component={ProjectEntry} />
+          <div
+            className="floating-back-button"
+            onClick={() => setSelectedProject(false)}
+          >
+            <p>{"<"}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
